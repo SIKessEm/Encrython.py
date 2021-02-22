8a86e61b1cfda512fa3b2897ccdfadf71b23ca0d
SKE-SIKessEm <sikessem@gmail.com>
2021-02-22 16:45:51 +0100

    Add encrypt and decrypt

--- Crypter.py
+++ Crypter.py
@@ -9,26 +9,56 @@ letters = string.ascii_letters
 letters = list(letters)
 lenght = len(letters)
 
+def encrypt(word, step):
+    code = ''
 
-code = ''
+    word_letters = list(word)
 
-word_letters = list(word)
+    for word_letter in word_letters:
 
-for word_letter in word_letters:
+        index = 0
 
-    index = 0
+        for letter in letters:
+            if letter == word_letter:
+                index = letters.index(letter)
+                break
 
-    for letter in letters:
-        if letter == word_letter:
-            index = letters.index(letter)
-            break
+        position = index + step
+        rest = lenght - position
 
-    position = index + step
-    rest = lenght - position
+        if rest < 0:
+            position = rest + step - 1
 
-    if rest < 0:
-        position = rest + step - 1
+        code += letters[position]
 
-    code += letters[position]
+    return code
+
+def decrypt(word, step):
+    code = ''
+
+    word_letters = list(word)
+
+    for word_letter in word_letters:
+
+        index = 0
+
+        for letter in letters:
+            if letter == word_letter:
+                index = letters.index(letter)
+                break
+
+        position = index - step
+
+        if position < 0:
+            position += lenght
+
+        code += letters[position]
+
+    return code
+
+
+
+
+code = decrypt(word, step)
 
 print('Le mot crypté est :', code)
