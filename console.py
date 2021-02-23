2897d0e2c77b471f30b8e80c35d6deb86ad1734a
SKE-SIKessEm <sikessem@gmail.com>
2021-02-23 04:27:03 +0100

    Use crypter module from console.py

--- console.py
+++ console.py
@@ -11,7 +11,7 @@
 
 
 
-import string
+from crypter import encrypt, decrypt
 
 print('>>>BIENVENUE DANS NOTRE PROGRAMME DE CRYPTAGE<<<')
 print('-------------------------------------------------')
@@ -21,75 +21,6 @@ word = input('Le mot à crypter : ')
 step = input('Le nombre de pas : ')
 
 step = int(step)
-
-
-letters = string.ascii_letters
-
-letters = list(letters)
-
-lenght = len(letters)
-
-
-def encrypt(word, step):
-
-    code = ''
-
-    word_letters = list(word)
-
-    for word_letter in word_letters:
-
-        index = 0
-
-        for letter in letters:
-
-            if letter == word_letter:
-
-                index = letters.index(letter)
-
-                break
-
-        position = index + step
-
-        rest = lenght - position
-
-        if rest < 0:
-
-            position = rest + step - 1
-
-        code += letters[position]
-
-    return code
-
-
-
-def decrypt(word, step):
-
-    code = ''
-
-    word_letters = list(word)
-
-    for word_letter in word_letters:
-
-        index = 0
-
-        for letter in letters:
-
-            if letter == word_letter:
-
-                index = letters.index(letter)
-
-                break
-
-        position = index - step
-
-        if position < 0:
-
-            position += lenght
-
-        code += letters[position]
-
-    return code
-
 
 action = ''
 
