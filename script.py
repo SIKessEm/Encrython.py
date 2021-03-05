9da86c457a03ca6ae48fb5f7da19175b36b13a05
SKE-SIKessEm <sikessem@gmail.com>
2021-03-05 14:41:13 +0100

    Use Window and show it

---
+++ script.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        sikessem-encryption
+# Purpose:
+#
+# Author:      SIGUI Kessé Emmanuel
+#
+# Created:     05/03/2021
+# Copyright:   (c) SIKessEm 2021
+# Licence:     MIT
+#-------------------------------------------------------------------------------
+
+
+
+from gui import Window
+
+
+win = Window('Encryption')
+
+
+win.show()
