62c4cbcdf78066c26608c56a2cd6e1a89c622c9c
SKE-SIKessEm <sikessem@gmail.com>
2021-03-05 15:35:51 +0100

    Add dostring for Window class

--- gui.py
+++ gui.py
@@ -14,10 +14,13 @@
 from tkinter import Tk
 
 class Window(Tk):
+    """The window class"""
 
     def __init__(self, title):
+        """Create a new window and add a title"""
         Tk.__init__(self)
         self.title(title)
 
     def show(self):
+        """Build and show the window"""
         return self.mainloop()
